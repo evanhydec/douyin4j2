5c9ee8af0a14b020d085dd212f5c25b301f8c739
evanhydec <evanhydec@163.com>
2022-06-23 14:27:50 +0800

    eureka

--- Eureka-7001/src/main/java/com/example/EurekaServer_7001.java
+++ Eureka-7001/src/main/java/com/example/EurekaServer_7001.java
@@ -1,2 +1,13 @@
-package com.example;public class EurekaServer_7001 {
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer_7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer_7001.class,args);
+    }
 }
