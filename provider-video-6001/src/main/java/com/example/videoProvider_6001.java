e5500f91c8824f576488eb7e133a1291b62e75af
evanhyde <evanhydec@163.com>
2022-06-24 17:24:23 +0800

    videos & favourites

--- provider-video-6001/src/main/java/com/example/videoProvider_6001.java
+++ provider-video-6001/src/main/java/com/example/videoProvider_6001.java
@@ -2,8 +2,14 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableEurekaClient
+@EnableFeignClients(basePackages = "com.example.service")
 public class videoProvider_6001 {
     public static void main(String[] args) {
         SpringApplication.run(videoProvider_6001.class,args);
