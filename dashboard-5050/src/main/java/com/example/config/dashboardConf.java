3b3b13681991bc1f043064087297b7ac6233c34d
evanhydec <evanhydec@163.com>
2022-06-25 09:56:26 +0800

    dashboard config

---
+++ dashboard-5050/src/main/java/com/example/config/dashboardConf.java
@@ -0,0 +1,16 @@
+package com.example.config;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class dashboardConf {
+    @Bean
+    public ServletRegistrationBean bean() {
+        ServletRegistrationBean bean = new ServletRegistrationBean(new HystrixMetricsStreamServlet());
+        bean.addUrlMappings("/actuator/hystrix.stream");
+        return bean;
+    }
+}
